b91a50d56246a9658ca47c5aebb8e246f3424f0d
Jonah <jonahtkane@gmail.com>
2023-03-30 00:18:28 -0500

    removed commeted out code

--- server.js
+++ server.js
@@ -1,55 +1,3 @@
-// const express = require('express');
-// const session = require('express-session');
-// const routes = require('./controllers');
-// const exphbs = require('express-handlebars');
-// const path = require('path');
-
-// //require middleware modules body-parser/cors
-// const bodyParser = require('body-parser');
-// const cors = require('cors');
-
-// const sequelize = require('./config/connection');
-// const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-// const app = express();
-// const PORT = process.env.PORT || 3001;
-// const hbs = exphbs.create({});
-
-// //add express-session as middle to express app
-// app.use(
-//   session({
-//     secret: "mysecretkey",
-//     resave: false,
-//     saveUninitialized: false
-//   })
-// );
-
-// app.engine('handlebars', hbs.engine);
-// app.set('view engine', 'handlebars');
-
-// const sess = {
-//   secret: 'Super secret secret',
-//   cookie: {},
-//   resave: false,
-//   saveUninitialized: true,
-//   store: new SequelizeStore({
-//     db: sequelize
-//   })
-// };
-
-// app.use(session(sess));
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(routes);
-
-// sequelize.sync({ force: false }).then(() => {
-//   app.listen(PORT, () => console.log('Now listening at http://localhost:3001'));
-// });
-
-
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
@@ -63,7 +11,6 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 const sess = {
@@ -83,7 +30,6 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
