341cfe56193858f1943d9e49f55e11fb3b789eb1
Jonah <jonahtkane@gmail.com>
2023-03-29 22:57:06 -0500

    removing commented out code

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,13 +3,9 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const movieRoutes = require('./movieRoutes');
 const homeRoutes = require('../homeRoutes');
-// const reviewsRoutes = require('../reviewsRoutes');
-// const aboutRoutes = require('../aboutRoutes');
 
 router.use('/users', userRoutes);
 router.use('/movies', movieRoutes);
 router.use('/home', homeRoutes);
-// router.use('/reviews', reviewsRoutes);
-// router.use('/about', aboutRoutes);
 
 module.exports = router;
