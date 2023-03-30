c9e4c8454fb2ebe4886b41ba375dd7ffdb242ea4
Jonah <jonahtkane@gmail.com>
2023-03-30 10:04:35 -0500

    updated class names

--- utils/helpers.js
+++ utils/helpers.js
@@ -9,11 +9,11 @@ module.exports = {
       const randomNum = Math.random();
   
       if (randomNum > 0.7) {
-        return `<span for="img" aria-label="lightbulb">🎞</span>`;
+        return `<span for="img" aria-label="film-strip">🎞</span>`;
       } else if (randomNum > 0.4) {
-        return `<span for="img" aria-label="laptop">📽</span>`;
+        return `<span for="img" aria-label="projector">📽</span>`;
       } else {
-        return `<span for="img" aria-label="gear">🎥</span>`;
+        return `<span for="img" aria-label="video-camera">🎥</span>`;
       }
     },
   };
